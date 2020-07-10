3e51de3413217ffba71c420d536275c494766781
fullatron <f20190104@goa.bits-pilani.ac.in>
2020-07-10 18:54:57 +0530

    Added all files

---
+++ model/predict.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+model = tf.keras.models.load_model('model/my_model.h5')
+def predict(data):
+    ###  IMAGE PREPROCESSING  ###
+
+    #  data-->preprocessed_data #
+
+    ###          END          ###
+    prediction = model.predict(preprocessed_data)
+
+    # You may want to further format the prediction to make it more
+    # human readable
+    return prediction
